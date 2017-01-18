dac2ea24b342d8c65e8f00f8836e8385491b8625
Allan de Almeida Vigiano <allanvigiano@gmail.com>
2017-01-18 12:51:16 -0200

    Final Commit

---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="! server">Please select a server.</p>
+        <div v-else>
+            <p>Id: {{server.id}}</p>
+            <p>Status: {{server.status}}</p>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    import { eventBus } from '../../main';
+    export default {
+        data() {
+            return {
+                    server: null,
+            };
+        },
+        created() {
+            eventBus.$on('serverSelected', (server) => {
+                this.server = server;
+            });
+
+        }
+    }
+</script>
+
+<style>
+</style>
